2d00e3830ec337947474d16dda5f4e994ad49f5a
mdalamin123a <md.alamindev90@gmail.com>
2024-10-25 00:56:57 +0600

    34_5-1-Ultra-Nation-Load-Countries-Data-Using-useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.countrylayer.com/v2/all?access_key=b39e2fdecb06fad84fbcd49ae78751e6")
+    .then(response => response.json())
+    .then(data => setCountries(data));
+  }, []);
+  
   return (
     <div className="App">
+      <h1>Country Loaded: {countries.length}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
